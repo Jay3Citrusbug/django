6b9cb2282fd4e27b999fc1066348db4a9fc10817
Jay <jay3.citrusbug@gmail.com>
2022-11-14 18:04:42 +0530

    14-11

--- demo/app1/urls.py
+++ demo/app1/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("",views.index,name="shophome")
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("signup/",views.sign_up,name="signup"),
+    path("",views.sign_in,name="signin")
+]  
 
--- demo/app1/views.py
+++ demo/app1/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+def sign_in(request):
+     return render(request,'sign_in.html')
+
+
+
+def sign_up(request):
+   return render(request,'signup.html')
